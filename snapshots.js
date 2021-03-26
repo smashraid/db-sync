b39fa36ed441a6b7c2422fc6f3c24a63a9adca71
stsuchida <Saulo.Tsuchida@cignium.com>
2021-03-26 14:18:37 -0500

    Fix Test

--- snapshots.js
+++ snapshots.js
@@ -1,15 +1,13 @@
 const PercyScript = require('@percy/script');
-const httpServer = require('http-server');
 
-const PORT = process.env.PORT_NUMBER || 8000;
-const TEST_URL = `http://localhost:${PORT}`;
+const TEST_URL = 'https://umb-bg.azurewebsites.net/';//`http://localhost:${PORT}`;
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
-  let server = httpServer.createServer();
-  server.listen(PORT);
+  //let server = httpServer.createServer();
+  //server.listen(PORT);
 
-  console.log(`Server started at ${TEST_URL}`);
+  //console.log(`Server started at ${TEST_URL}`);
 
   await page.goto(TEST_URL);
   await percySnapshot('Home');
@@ -19,5 +17,5 @@ PercyScript.run(async (page, percySnapshot) => {
   //await page.waitFor('.section__hero-content');
   //await page.keyboard.press('Enter');
   //await percySnapshot('Home', { widths: [768, 992, 1200] });
-  server.close();
+  //server.close();
 });
